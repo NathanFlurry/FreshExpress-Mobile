bf94978940b0a9f9e3ffd943a7ac9bdf3e4f0479
Nathan Flurry <bitbucket@nathanflurry.com>
2016-07-22 14:22:19 -0700

    - Fixed initial view controller

---
+++ FreshExpress/Server.swift
@@ -0,0 +1,14 @@
+//
+//  Server.swift
+//  FreshExpress
+//
+//  Created by Nathan Flurry on 7/22/16.
+//  Copyright © 2016 NathanFlurry. All rights reserved.
+//
+
+import Foundation
+
+class Server {
+	static let serverAddress = "http://localhost:8080/"
+	
+}
